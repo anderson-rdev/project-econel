0b333f6da6d67616ba4f51643c975918562d9659
Anderson Ramos <an2767394@gmail.com>
2025-09-28 15:29:32 -0300

    feat(Pessoa): Criando classe pessoa

---
+++ Management/src/main/java/com/management/model/Pessoa.java
@@ -0,0 +1,41 @@
+package com.management.model;
+
+public class Pessoa {
+    private Long id;
+    private String nome;
+
+    // Construtor padrão
+    public Pessoa() {}
+
+    // Construtor com parâmetros
+    public Pessoa(Long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    // toString para facilitar exibição
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                '}';
+    }
+}
